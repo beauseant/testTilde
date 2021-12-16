785fb1a279488f4aff7d8cd9ac3dbc97b2ba0cad
Beauseant <sblanco@tsc.uc3m.es>
2021-12-16 15:28:40 +0100

    api de traducción incorporada en una clase llamada rosseta
    Signed-off-by: Beauseant <sblanco@tsc.uc3m.es>

--- main.py
+++ main.py
@@ -8,28 +8,28 @@ import multiprocessing
 
 def readJson (f):
 
-	response = f.read()
-	response = response.replace('\n', '')
-	response = response.replace('}{', '},{')
-	response = "[" + response + "]"	
-	return json.loads(response)
+    response = f.read()
+    response = response.replace('\n', '')
+    response = response.replace('}{', '},{')
+    response = "[" + response + "]" 
+    return json.loads(response)
 
 
 
 def loadFile (file):
-	listAbstract = list ()
-	try:
-		with open( file ) as f:
-			data = list(filter (lambda articulo : articulo['paperAbstract'] != '', readJson (f)[:3000]))
-			listAbstract = random.sample (data, argus.abstract)
-			print ('finish with %f' % file)
-	except:
-		print ('error leyendo %s' % file)
+    listAbstract = list ()
+    try:
+        with open( file ) as f:
+            data = list(filter (lambda articulo : articulo['paperAbstract'] != '', readJson (f)[:12000]))
+            listAbstract = random.sample (data, argus.abstract)
+            print ('finish with %s' % file)
+    except Exception as E:
+        print ('error leyendo %s, %s' % file, str(E))
 
-	return listAbstract
+    return listAbstract
 
 def translate (articulo ):
-	return len(re.findall(r'\w+', articulo['paperAbstract']))
+    return len(re.findall(r'\w+', articulo['paperAbstract']))
 
 
 
@@ -37,78 +37,77 @@ def translate (articulo ):
 if __name__ == "__main__":
 
 
-	parser = argparse.ArgumentParser(description='Script en python para pasar openaire a psql')
-	parser.add_argument('-t','--type', help='Tipo de textos a probar, scolar', required=True, choices=['scolar'])
-	parser.add_argument('-p','--path', help='Ruta con los ficheros', required=True )
-	parser.add_argument('-n','--numero', help='Número de archivos a procesar', required=True, type=int)
-	parser.add_argument('-na','--abstract', help='Número de abstract de cada archivo a procesar', required=True, type=int)
-	parser.add_argument('-th','--threads', required=True, type=int, help='hilos de ejecución a lanzar,1 secuencial')
-	argus = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Script en python para pasar openaire a psql')
+    parser.add_argument('-t','--type', help='Tipo de textos a probar, scolar', required=True, choices=['scolar'])
+    parser.add_argument('-p','--path', help='Ruta con los ficheros', required=True )
+    parser.add_argument('-n','--numero', help='Número de archivos a procesar', required=True, type=int)
+    parser.add_argument('-na','--abstract', help='Número de abstract de cada archivo a procesar', required=True, type=int)
+    parser.add_argument('-th','--threads', required=True, type=int, help='hilos de ejecución a lanzar,1 secuencial')
+    argus = parser.parse_args()
 
 
 
 
-	token = 'u-fc6f1588-4dc0-4358-aa48-106749f327af'
-	url = 'https://www.letsmt.eu/ws/service.svc/json/GetSystemList?appID=myappid'
+    token = 'u-fc6f1588-4dc0-4358-aa48-106749f327af'
+    url = 'https://www.letsmt.eu/ws/service.svc/json/GetSystemList?appID=myappid'
 
 
-	'''
-	post_params = { 'client-id' : token }
+    '''
+    post_params = { 'client-id' : token }
 
-	query_string = urllib.parse.urlencode( post_params ) 
+    query_string = urllib.parse.urlencode( post_params ) 
 
-	data = query_string.encode( "ascii" )    
-
- 
-	url = url + "?" + query_string 
+    data = query_string.encode( "ascii" )    
 
  
-	with urllib.request.urlopen( url ) as response: 
-		data = response.read() 
+    url = url + "?" + query_string 
 
-	'''
+ 
+    with urllib.request.urlopen( url ) as response: 
+        data = response.read() 
 
-	import urllib3
+    '''
 
-	http = urllib3.PoolManager()
-	myHeaders = urllib3.util.make_headers(basic_auth='client-id:u-fc6f1588-4dc0-4358-aa48-106749f327af')
-	resp = http.request('GET', url, headers=myHeaders)
-	data = resp.data.decode('utf-8')
+    import urllib3
 
-	
+    http = urllib3.PoolManager()
+    myHeaders = urllib3.util.make_headers(basic_auth='client-id:u-fc6f1588-4dc0-4358-aa48-106749f327af')
+    resp = http.request('GET', url, headers=myHeaders)
+    data = resp.data.decode('utf-8')
 
-
-	
-
-	if argus.type == 'scolar':
-
-		#creamos una lista con todos los ficheros de scolar:
-		scopusFiles = glob.glob( argus.path + '/*corpus*' )
-		#tomamos una muestra aleatoria sin repetirlos:
-		selectScopusFiles = random.sample( scopusFiles , k=argus.numero)
+    
 
 
-		listAbstract = list()
+    
 
-		with multiprocessing.Pool(processes= int(argus.threads)) as pool:
-			listAbstract = pool.starmap(loadFile, zip(selectScopusFiles))
+    if argus.type == 'scolar':
 
-		import ipdb ; ipdb.set_trace()
-
-
-		numWords = map (lambda articulo : len(re.findall(r'\w+', articulo['paperAbstract'])), listAbstract)
-		print ('número de articulos procesados: %s' % len (listAbstract))
-		print ('número de palabras totales: %s' % sum(numWords))
+        #creamos una lista con todos los ficheros de scolar:
+        scopusFiles = glob.glob( argus.path + '/*corpus*' )
+        #tomamos una muestra aleatoria sin repetirlos:
+        selectScopusFiles = random.sample( scopusFiles , k=argus.numero)
 
 
-	with multiprocessing.Pool(processes= int(argus.threads)) as pool:
-		results = pool.starmap(translate, zip(listAbstract))
+        listAbstract = list()
 
-	import ipdb ; ipdb.set_trace()
+        with multiprocessing.Pool(processes= int(argus.threads)) as pool:
+            listAbstract = pool.starmap(loadFile, zip(selectScopusFiles))
+        listAbstract = [item for sublist in listAbstract for item in sublist]
+
+
+        numWords = map (lambda articulo : len(re.findall(r'\w+', articulo['paperAbstract'])), listAbstract)
+        print ('número de articulos procesados: %s' % len (listAbstract))
+        print ('número de palabras totales: %s' % sum(numWords))
+
+
+    with multiprocessing.Pool(processes= int(argus.threads)) as pool:
+        results = pool.starmap(translate, zip(listAbstract))
+
+    import ipdb ; ipdb.set_trace()
 
 
 
-	
+    
 
 
 
